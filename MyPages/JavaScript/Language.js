81e4b292907980d840891b1f855225d30970b937
funtomi <396166153@qq.com>
2017-12-07 17:05:21 +0800

    submit language

--- MyPages/JavaScript/Language.js
+++ MyPages/JavaScript/Language.js
@@ -42,7 +42,7 @@ function translate() {
     $("[lang]").each(function () {
         switch (this.tagName.toLowerCase()) {
             case "input":
-                $(this).val(__tr($(this).attr("lang")));
+                $(this).attr("placeholder", __tr($(this).attr("lang")));
                 break;
             default:
                 $(this).text(__tr($(this).attr("lang")));
@@ -56,38 +56,21 @@ function __tr(src) {
 
 function loadDict() {
     var lang = (getCookieVal("lang") || "en");
-    $.getJSON(lang + ".json", function (data) {
-        $.ajax({
-            async: false,
-            type: "GET",
-            url: lang + ".json",
-            dataType: "json",
-            success: function (msg) {
-                alert("success");
-            },
-            error: function (msg) {
-                alert(msg);
-            }
-        });
+    $.ajax({
+        async: false,
+        type: "GET",
+        url: lang + ".json",
+        success: function (msg) {
+            dict = msg;
+        },
     });
-    //    dict = eval("(" + msg + ")");
-    //    alert("success");
-    //});
-    //$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=?", function (data) {
-    //    alert("success");
-    //    $.each(data.items, function (i, item) {
-    //        $("<img/>").attr("src", item.media.m).appendTo("#images");
-    //        if (i == 3) return false;
-    //    });
-//});
-    //});
 }
 
 function registerWords() {
     $("[lang]").each(function () {
         switch (this.tagName.toLowerCase()) {
             case "input":
-                $(this).attr("lang", $(this).val());
+                $(this).attr("lang", $(this).attr("placeholder"));
                 break;
             default:
                 $(this).attr("lang", $(this).text());
